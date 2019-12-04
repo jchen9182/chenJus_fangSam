6a23bee16401f96303d3513d4e8d9d870a5de07c
SamuelFang <sfang00@stuy.edu>
2019-12-04 11:02:20 -0500

    wait

--- run.c
+++ run.c
@@ -3,10 +3,12 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "headers.h"
 
 char ** parse_args(char *line) {
-    char ** arr = malloc(sizeof(char*) * 5);
+    char ** arr = malloc(sizeof(char*) * 10);
     int counter = 0;
 
     while (line != NULL) {
@@ -21,12 +23,23 @@ char ** parse_args(char *line) {
 
 int main() {
     char input[100];
+    char **args;
+    int status;
     while (1) {
+        printf("> ");
         fgets(input, 100, stdin);
         for (int i = 0; i < 100; i++) {
             if (input[i] == '\n') input[i] = '\0';
         }
-        printf("%s", input);
         if (!strcmp(input, "exit")) break;
+        args = parse_args(input);
+        int pid = fork();
+        if (pid < 0){
+            printf("error %s\n",strerror(errno));
+        }
+        if (pid == 0){
+            printf("WAIT");
+            wait(&status);
+        }
     }
 }

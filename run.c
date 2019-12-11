3857c2190b1a7414c27740a674ee80c7d7f77673
SamuelFang <sfang00@stuy.edu>
2019-12-10 23:06:59 -0500

    semicolon

--- run.c
+++ run.c
@@ -20,3 +20,52 @@ char ** parse_args(char *line) {
     arr[counter] = NULL;
     return arr;
 }
+
+char ** semicolon_split(char *line) {
+    char ** arr = malloc(sizeof(char*) * 10);
+    int counter = 0;
+    if (strchr(line, *";") == NULL) {
+        char* str = strsep(&line, ";");
+        arr[counter] = malloc(strlen(str));
+        arr[counter] = str;
+        counter++; 
+    }
+    else {
+        while (line != NULL) {
+            if (counter == 0){
+                char* str = strsep(&line, ";");
+                char* newstr = malloc(strlen(str) - 1);
+                arr[counter] = malloc(strlen(str) - 1);
+                for (int i = 0; i < (strlen(str) - 1); i++) {
+                    newstr[i] = str[i];
+                }
+                arr[counter] = newstr;
+                counter++;
+            }
+            else {
+                if (strchr(line, *";") == NULL) {
+                    char* str = strsep(&line, ";");
+                    char* newstr = malloc(strlen(str) - 1);
+                    arr[counter] = malloc(strlen(str) - 1);
+                    for (int i = 0; i < (strlen(str) - 1); i++) {
+                        newstr[i] = str[i + 1];
+                    }
+                    arr[counter] = newstr;
+                    counter++;
+                }
+                else {
+                    char* str = strsep(&line, ";");
+                    char* newstr = malloc(strlen(str) - 2);
+                    arr[counter] = malloc(strlen(str) - 2);
+                    for (int i = 0; i < (strlen(str) - 2); i++) {
+                        newstr[i] = str[i + 1];
+                    }
+                    arr[counter] = newstr;
+                    counter++;
+                }
+            }
+        }
+    }
+    arr[counter] = NULL;
+    return arr;
+}

c6bfd03d25b3c600e55b26f6acbb9f3420553b42
SamuelFang <47552568+SamuelFang@users.noreply.github.com>
2019-12-15 14:37:51 -0500

    Update headers.h

--- headers.h
+++ headers.h
@@ -1,5 +1,6 @@
 char ** parse_args(char *line);
 char ** semicolon_split(char *line);
+char * take_out(char line[]);
 void print_head();
 char * remove_newline(char *line);
 void execute(char **args);

2e577fc62e578b9d35502cb57106461094e0967b
SamuelFang <47552568+SamuelFang@users.noreply.github.com>
2019-12-15 14:37:12 -0500

    Update run.c

--- run.c
+++ run.c
@@ -6,12 +6,14 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "headers.h"
+#include <ctype.h>
 
 char ** parse_args(char *line) {
     char ** arr = malloc(sizeof(char*) * 10);
     int counter = 0;
 
     while (line != NULL) {
+        line = take_out(line);
         char* str = strsep(&line, " ");
         arr[counter] = malloc(strlen(str));
         arr[counter] = str;
@@ -24,50 +26,48 @@ char ** parse_args(char *line) {
 char ** semicolon_split(char *line) {
     char ** arr = malloc(sizeof(char*) * 10);
     int counter = 0;
-    if (strchr(line, *";") == NULL) {
+
+    while (line != NULL) {
         char* str = strsep(&line, ";");
         arr[counter] = malloc(strlen(str));
-        arr[counter] = str;
-        counter++; 
-    }
-    else {
-        while (line != NULL) {
-            if (counter == 0){
-                char* str = strsep(&line, ";");
-                char* newstr = malloc(strlen(str) - 1);
-                arr[counter] = malloc(strlen(str) - 1);
-                for (int i = 0; i < (strlen(str) - 1); i++) {
-                    newstr[i] = str[i];
-                }
-                arr[counter] = newstr;
-                counter++;
-            }
-            else {
-                if (strchr(line, *";") == NULL) {
-                    char* str = strsep(&line, ";");
-                    char* newstr = malloc(strlen(str) - 1);
-                    arr[counter] = malloc(strlen(str) - 1);
-                    for (int i = 0; i < (strlen(str) - 1); i++) {
-                        newstr[i] = str[i + 1];
-                    }
-                    arr[counter] = newstr;
-                    counter++;
-                }
-                else {
-                    char* str = strsep(&line, ";");
-                    char* newstr = malloc(strlen(str) - 2);
-                    arr[counter] = malloc(strlen(str) - 2);
-                    for (int i = 0; i < (strlen(str) - 2); i++) {
-                        newstr[i] = str[i + 1];
-                    }
-                    arr[counter] = newstr;
-                    counter++;
-                }
-            }
-        }
+
+        arr[counter] = take_out(str);
+        counter++;
     }
     arr[counter] = NULL;
     return arr;
+}
+
+char * take_out(char line[]) {
+    int i = 0;
+    int num = 0;
+    for (i = 0; line[i] != 0; i++){
+        if (isspace(line[i])){
+            num++;
+        }
+        else{
+            break;
+        }
+    }
+    char* newstr = malloc(strlen(line) - num);
+    for (int i = 0; i < (strlen(line) - num); i++) {
+        newstr[i] = line[i + num];
+    }
+    num = 0;
+    for (i = strlen(newstr) - 1; line[i] != 0; i--){
+        if (isspace(newstr[i])){
+            num++;
+        }
+        else{
+            break;
+        }
+    }
+    char* newstr2 = malloc(strlen(newstr) - num);
+    for (int i = 0; i < (strlen(newstr) - num); i++) {
+        newstr2[i] = newstr[i];
+    }
+    return newstr2;
+
 }
 
 void print_head() {
